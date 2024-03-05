3a671372c068280b43d9146e63d67b991ca48db5
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2024-03-05 21:24:55 +0530

    reviewed all the questions through feedback video

---
+++ Assignment: Loops/index.js
@@ -0,0 +1,18 @@
+//  Loop basics
+
+// Do the following using loops:
+
+// Using `console.log` log all the values from 1 to 10.
+
+for (let i = 1; i < 11; i++){
+    console.log(i)
+}
+
+// Using `console.log` log all the evan values from 1 to 10.
+
+// Using `console.log` log all the od values from 1 to 10.
+
+
+// Calculate the sum of all numbers from 1 to 10.
+
+// Log all the values from 1 to 10 using while loop
